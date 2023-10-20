5c91f772996a0aafb48fa9fabe8bf1b018e1fe35
Harrison Bounds <harrison.bounds777@gmail.com>
2023-10-20 12:50:24 -0500

    using dask to load data

--- trying_dask.py
+++ trying_dask.py
@@ -1,22 +1,45 @@
-import pandas as pd
-import numpy as np
 import dask.dataframe as dd
-import glob
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from dask_ml.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
 path = 'E:\CICIoT2023'
 
 df = dd.read_csv(path + '\*.csv')
 
-print("Number of rows: ", len(df))
-labels, uniques = pd.factorize(df['label'])
+#column that contains the labels
+label_column = 'label'
 
-# sampled_rows = df.sample(frac=3/len(df)).compute()
+X_train, X_test, y_train, y_test = train_test_split(df, df[label_column], test_size=0.20, shuffle=True)
 
-# print("Sample: ", sampled_rows)
-print(f'There are {len(uniques)} labels: {uniques}')
+#Drop the labels from the training set
+X_train = X_train.drop(label_column, axis=1)
+X_test = X_test.drop(label_column, axis=1)
+
+#Covert the text labels to numbers so the model can understand it
+label_encoder = LabelEncoder()
+
+y_train_encoded = label_encoder.fit_transform(y_train)
+y_test_encoded = label_encoder.fit_transform(y_test)
+
+def view_data():
+    #Checking the results
+    print("Total rows: ", len(df))
+    print("X_train shape: ", X_train.shape[0].compute())
+    print("X_test shape: ", X_test.shape[0].compute())
+    print("y_train shape: ", y_train.shape[0].compute())
+    print("y_test shape: ", y_test.shape[0].compute())
+
+    print("Examples\n")
+    print("X_train example: ", X_train.head())
+    print("X_test example: ", X_test.head())
+    print("y_train example: ", y_train.head())
+    print("y_test example: ", y_test.head())
+
+view_data()
+
+
+
 
 
 
