5c868c03466229aba1a5e9a97bf916b045b4fef6
Harrison Bounds <harrison.bounds777@gmail.com>
2024-02-02 10:00:43 -0600

    adding per class mertics to iot

--- iot.py
+++ iot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -24,44 +24,6 @@ def encode_labels(y_train, y_test):
 
     return y_train_encoded, y_test_encoded, class_names
 
-def generate_data(df, gan_model, num_generated_rows, label_column, path):
-    if gan_model == 'ctgan':
-        #Convert dataframe into metadata
-        metadata = SingleTableMetadata()
-        metadata.detect_from_dataframe(df)
-
-        #Generate synthetic data
-        print("Generating Synthetic Data...")
-        start_time = time.time()
-        synthesizer = CTGANSynthesizer(metadata, epochs=1000, verbose=True)
-        synthesizer.fit(df)
-        end_time = time.time()
-
-        print(f'It took {(end_time-start_time)/60} minutes to generate')
-
-
-        synthetic_data = synthesizer.sample(num_rows=num_generated_rows)
-    elif gan_model == 'ctabgan':
-        ctabgan_instance = ctabgan.CTABGAN(path, 0.2, [], [], [], ['pslist.nproc', 'pslist.nppid', 'pslist.avg_threads',	'pslist.nprocs64bit', 'pslist.avg_handlers', 'dlllist.ndlls',
-                                                    'dlllist.avg_dlls_per_proc',	'handles.nhandles',	'handles.avg_handles_per_proc',	'handles.nport',	'handles.nfile',	
-                                                    'handles.nevent',	'handles.ndesktop',	'handles.nkey',	'handles.nthread',	'handles.ndirectory',	'handles.nsemaphore',	
-                                                    'handles.ntimer',	'handles.nsection',	'handles.nmutant',	'ldrmodules.not_in_load',	'ldrmodules.not_in_init',	
-                                                    'ldrmodules.not_in_mem',	'ldrmodules.not_in_load_avg',	'ldrmodules.not_in_init_avg',	'ldrmodules.not_in_mem_avg',	
-                                                    'malfind.ninjections',	'malfind.commitCharge',	'malfind.protection',	'malfind.uniqueInjections',	'psxview.not_in_pslist',	
-                                                    'psxview.not_in_eprocess_pool',	'psxview.not_in_ethread_pool',	'psxview.not_in_pspcid_list',	'psxview.not_in_csrss_handles',	
-                                                    'psxview.not_in_session',	'psxview.not_in_deskthrd',	'psxview.not_in_pslist_false_avg',	'psxview.not_in_eprocess_pool_false_avg', 'psxview.not_in_ethread_pool_false_avg',	
-                                                    'psxview.not_in_pspcid_list_false_avg',	'psxview.not_in_csrss_handles_false_avg', 'psxview.not_in_session_false_avg',	'psxview.not_in_deskthrd_false_avg',	
-                                                    'modules.nmodules',	'svcscan.nservices',	'svcscan.kernel_drivers	svcscan.fs_drivers',	'svcscan.process_services',	'svcscan.shared_process_services',	
-                                                    'svcscan.interactive_process_services',	'svcscan.nactive',
-                                                    'callbacks.ncallbacks',	'callbacks.nanonymous',	'callbacks.ngeneric'], { "Classification": label_column }, 100)
-
-        ctabgan_instance.fit()
-        synthetic_data = ctabgan_instance.generate_samples()
-
-    else:
-        synthetic_data = None
-
-    return synthetic_data
 
 def binary_classification(X_train, y_train, X_test, y_test, num_trees):
     positive_class = "BenignTraffic"
@@ -107,11 +69,26 @@ def evaluate(y_test, y_pred, y_pred_prob, class_names):
 
     accuracy = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    precision = precision_score(y_test, y_pred, average='weighted')
 
     print(f'Accuracy: {accuracy}')
-    # print(f'Precision: {precision}')
-    # print(f'Recall Score: {recall}')
-    print(f'f1 Score: {f1}')
+    print(f'Precision: {precision}')
+    print(f'Recall Score: {recall}')
+    print(f'f1 Score: {f1}\n')
+
+    for i, c in enumerate(class_names):
+        class_accuracy = accuracy_score(y_test == i, y_pred == i)
+        class_f1 = f1_score(y_test == i, y_pred == i)
+        class_recall = recall_score(y_test == i, y_pred == i)
+        class_precision = precision_score(y_test == i, y_pred == i)
+
+        print(f'Class: {c}')
+        print(f'  Accuracy: {class_accuracy}')
+        print(f'  Precision: {class_precision}')
+        print(f'  Recall: {class_recall}')
+        print(f'  f1: {class_f1}\n')
+        
 
 def show_confusion_matrix(y_test, y_pred, class_names, num_trees):
     confusion_mat = confusion_matrix(y_test, y_pred)
