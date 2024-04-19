358fbc79b1ebc8f6b3cea345edc59e3b35b05b80
Harrison Bounds <harrison.bounds777@gmail.com>
2024-04-19 11:14:23 -0500

    implement cwgan

--- CasTGAN/Model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .BaseGAN import BaseGAN
-from .CasTGAN import CasTGAN
-#from .CasTGAN_bleh import CasTGAN
-#from .AutoInt import AutoInt
-from .DataPrep import DataPrep
-#from .ModelEvaluation import models_evaluator
